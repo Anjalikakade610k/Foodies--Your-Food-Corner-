387ddb6eed9b9fa37fd7a99ff45cd5d663e7e304
Anjalikakade610k <71441240+Anjalikakade610k@users.noreply.github.com>
2024-09-27 13:24:27 -0700

    Update

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+
+export const IMG_CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2";
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
+  export const LOGO = "https://icon-library.com/images/food-app-icon/food-app-icon-0.jpg";
+  export const SEARCH_LOGO = "https://tse2.mm.bing.net/th?id=OIP.47jxyhFA1ibPiI_wg7KaDQHaHL&pid=Api&P=0&h=220";
+  export const LOGIN = "https://tse4.mm.bing.net/th?id=OIP.jAioCeiE6Cwhq9Ph3dee4gHaHa&pid=Api&P=0&h=220";
+  export const STAR = "https://tse2.mm.bing.net/th?id=OIP.Vq4iQC8p3quhWYipAjoIIAAAAA&pid=Api&P=0&w=300&h=300";
+  export const LINKEDIN = "https://www.linkedin.com/in/anjali-kakade/";
+  export const FOOD = "https://www.annmariegianni.com/wp-content/uploads/2014/05/brain-foods-1.jpg";
+  export const CONTACT = "https://img.freepik.com/premium-vector/young-man-using-laptop-computer-desk-working-online-illustration_24877-69116.jpg?w=2000";
+  export const EMPTY = "https://tse2.mm.bing.net/th?id=OIP.VEtjKjhwDm_M6AXOQmbZbwHaEX&pid=Api&P=0&h=220"; 
+  export const BACKEND_URL = "https://foodieco-backend-216f2650ef56.herokuapp.com";
+  export const RESTAURANT_LIST = `${BACKEND_URL}/api/restaurants`;
+  export const MENU_URL = `${BACKEND_URL}/api/menu`;
+  export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+  
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+    // content: ["./src/**/*.{html,js}"],
+    content: ['./*.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          customGreen: '#cdd15e',
+        },
+        fontFamily: {
+          cursive: ['cursive'],
+        },
+        backgroundImage: {
+          customimage: "url('https://www.annmariegianni.com/wp-content/uploads/2014/05/brain-foods-1.jpg')",
+        },
+        
+      },
+    },
+    plugins: [],
+  }
+  
+  
